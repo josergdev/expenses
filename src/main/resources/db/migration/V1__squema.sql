524761680296e3bbc9c166701358e893b0b5be6f
Jose Rodriguez <jose.rodriguezgarcia@enreach.com>
2022-08-13 19:26:53 +0200

    Add initial mysql datasource

---
+++ src/main/resources/db/migration/V1__squema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS friend
+(
+    id   VARCHAR(36)  NOT NULL UNIQUE PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS expense
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    payer_id    VARCHAR(36)  NOT NULL,
+    amount      BIGINT       NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
